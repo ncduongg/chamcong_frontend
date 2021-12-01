83e5e34169ad5787d2d8715e6250ebc1a6a8ec29
ncduongg <ncduongg@gmail.com>
2021-12-01 17:04:07 +0700

    initial prj

--- src/api/apiAdmin.js
+++ src/api/apiAdmin.js
@@ -1,66 +1,66 @@
-import axiosClients from "./axiosClients"
-import qs from "qs"
-import axios from "axios"
+import axiosClients from "./axiosClients";
+import qs from "qs";
+import axios from "axios";
 const apiAdmin = {
-    login(data) {
-        const url = 'apiAdmin/loginAdmin'
-        return axiosClients.post(url, data)
+  login(data) {
+    const url = "apiAdmin/loginAdmin";
+    return axiosClients.post(url, data);
+  },
+  getall(id, dateStart, dateEnd, idLocal) {
+    try {
+      const dateStartNew = new Date(dateStart).toISOString().split("T")[0];
+      const newDateEndOld = new Date(dateEnd).toISOString().split("T")[0];
+      //let dateEndOld = new Date(newDateEndOld.setDate(newDateEndOld.getDate() + 1)).toISOString().split("T")[0];
+      const url = `/api/filter?id=${id}&dateStart=${dateStartNew}T00:00:01.000Z&dateEnd=${newDateEndOld}T23:59:00.000Z&idLocal=${idLocal}`;
+      return axiosClients.get(url);
+    } catch (error) {
+      return error;
     }
-    ,
-    getall(id, dateStart, dateEnd,idLocal) {
-        try {
-            const dateStartNew = new Date(dateStart).toISOString().split("T")[0];
-            const newDateEndOld = new Date(dateEnd).toISOString().split("T")[0];
-            //let dateEndOld = new Date(newDateEndOld.setDate(newDateEndOld.getDate() + 1)).toISOString().split("T")[0];
-            const url = `/api/filter?id=${id}&dateStart=${dateStartNew}T00:00:01.000Z&dateEnd=${newDateEndOld}T23:59:00.000Z&idLocal=${idLocal}`
-            return axiosClients.get(url)
-        } catch (error) {
-            return error
-        }
-    },
-    getIDandUser(params) {
-        try {
-            const url = 'apiAdmin/getIdAndName'
-            const newParams  =  [params]
-            return axiosClients.post(url,newParams)
-        } catch (error) {
-            return error
-        }
-    },
-    uploadFile(file) {
-        try {
-            const url = 'apiAdmin/uploadfile'
-            const formData = new FormData()
-            formData.append('file', file)
-            return axiosClients.post(url, formData)
-        } catch (error) {
-            return error
-        }
-    },
-    getListFile() {
-        const url = 'apiAdmin/getListFile'
-        return axiosClients.get(url)
-    },
-    readFile(params) {
-        const url = 'apiAdmin/readfileNOsql'
-        return axiosClients.get(url, {
-            params: params, paramsSerializer: params => {
-                return qs.stringify(params)
-            }
-        })
-    },
-    writeFile(params) {
-        const url = 'apiAdmin/writeFiletoCSDL'
-        console.log(params);
-        return axiosClients.post(url, params)
-    },
-    getListVanPhong() {
-        const url = 'apiAdmin/getListVP'
-        return axiosClients.get(url)
-    },
-    addListVanPhong(data) {
-        const url = 'apiAdmin/addListVP'
-        return axiosClients.post(url, data)
+  },
+  getIDandUser(params) {
+    try {
+      const url = "apiAdmin/getIdAndName";
+      const newParams = [params];
+      return axiosClients.post(url, newParams);
+    } catch (error) {
+      return error;
     }
-}
-export default apiAdmin
+  },
+  uploadFile(file) {
+    try {
+      const url = "apiAdmin/uploadfile";
+      const formData = new FormData();
+      formData.append("file", file);
+      return axiosClients.post(url, formData);
+    } catch (error) {
+      return error;
+    }
+  },
+  getListFile() {
+    const url = "apiAdmin/getListFile";
+    return axiosClients.get(url);
+  },
+  readFile(params) {
+    const url = "apiAdmin/readfileNOsql";
+    return axiosClients.get(url, {
+      params: params,
+      paramsSerializer: (params) => {
+        return qs.stringify(params);
+      },
+    });
+  },
+  writeFile(params) {
+    const url = "apiAdmin/writeFiletoCSDL";
+    console.log(params);
+    return axiosClients.post(url, params);
+  },
+  getListVanPhong() {
+    const url = "apiAdmin/getListVP";
+    return axiosClients.get(url);
+  },
+  addListVanPhong(data) {
+    const url = "apiAdmin/addListVP";
+    return axiosClients.post(url, data);
+  },
+};
+export default apiAdmin;
