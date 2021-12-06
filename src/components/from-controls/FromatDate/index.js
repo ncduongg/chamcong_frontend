be7fc22bbc8376cd189c1b76940e8f6e9739d678
ncduongg <ncduongg@gmail.com>
2021-12-06 14:56:44 +0700

    ok

--- src/components/from-controls/FromatDate/index.js
+++ src/components/from-controls/FromatDate/index.js
@@ -24,7 +24,6 @@ const Thang = {
 };
 module.exports.formatDateTimeVN = (time) => {
   const timeNew = moment(time).toString();
-  console.log(timeNew);
   const timeNewString = timeNew.split(" ");
   const objTimeNew = {
     thu: Thu[timeNewString[0]],
